b3d2cbf94221d81a709e0236a35a7ab82e5dc072
Your Name <you@example.com>
2022-12-12 10:37:07 -0600

    Finished excercise

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -1,32 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'candidates',
-  templateUrl: './candidates.component.html',
-  styleUrls: ['./candidates.component.css']
+  selector: "candidates",
+  templateUrl: "./candidates.component.html",
+  styleUrls: ["./candidates.component.css"],
 })
 export class CandidatesComponent implements OnInit {
-   missionName = "LaunchCode Moonshot"
+  missionName = "LaunchCode Moonshot";
 
-   candidates = [
-    {name: 'Rusty Rutabaga', data: {age: 5, mass: '0.75 kg', sidekick: 'Blake'}, image: 'assets/images/Blake.png'},
-    {name: 'Tessa Tortoise', data: {age: 126, mass: '113 kg', sidekick: 'Sally'}, image: 'assets/images/Sally.png'},
-    {name: 'Bernie Beagle', data: {age: 4, mass: '15 kg', sidekick: 'Paul'}, image: 'assets/images/Paul.png'},
-    {name: 'Jackalope', data: {age: 45, mass: '33 kg', sidekick: 'Chris'}, image: 'assets/images/Chris.png'},
-    {name: 'Champ', data: {age: 9, mass: '36 kg', sidekick: 'Carly'}, image: 'assets/images/Carly.png'}
+  candidates = [
+    {
+      name: "Rusty Rutabaga",
+      data: { age: 5, mass: "0.75 kg", sidekick: "Blake" },
+      image: "assets/images/Blake.png",
+    },
+    {
+      name: "Tessa Tortoise",
+      data: { age: 126, mass: "113 kg", sidekick: "Sally" },
+      image: "assets/images/Sally.png",
+    },
+    {
+      name: "Bernie Beagle",
+      data: { age: 4, mass: "15 kg", sidekick: "Paul" },
+      image: "assets/images/Paul.png",
+    },
+    {
+      name: "Jackalope",
+      data: { age: 45, mass: "33 kg", sidekick: "Chris" },
+      image: "assets/images/Chris.png",
+    },
+    {
+      name: "Champ",
+      data: { age: 9, mass: "36 kg", sidekick: "Carly" },
+      image: "assets/images/Carly.png",
+    },
   ];
 
   crew = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   // Code the addToCrew function here:
-
+  addToCrew(person: object) {
+    if (!this.crew.includes(person)) {
+      this.crew.push(person);
+    }
+  }
 
   // BONUS: Code the changeMissionName function here:
-
-
+  changeMissionName(newName: string) {
+    this.missionName = newName;
+    return false;
+  }
 }
