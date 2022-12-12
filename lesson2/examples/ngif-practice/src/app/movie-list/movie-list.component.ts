9aa9ed2341381926c80a3fe554ca1c65d5d711b9
Your Name <you@example.com>
2022-12-11 23:09:38 -0600

    finished examples

--- lesson2/examples/ngif-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/ngif-practice/src/app/movie-list/movie-list.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'movie-list',
-  templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.css']
+  selector: "movie-list",
+  templateUrl: "./movie-list.component.html",
+  styleUrls: ["./movie-list.component.css"],
 })
 export class MovieListComponent implements OnInit {
-  movies = ['The Manchurian Candidate', 'Oceans 8', 'Hidden Figures', 'The Incredibles', 'The Princess Bride'];
+  movies = [
+    "The Manchurian Candidate",
+    "Oceans 8",
+    "Hidden Figures",
+    "The Incredibles",
+    "The Princess Bride",
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
